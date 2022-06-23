92501f5a82d45ef18a976d66e67d13e04f665d08
Jaesung Hong <jezsung@gmail.com>
2022-06-23 10:42:34 -0500

    add error handlers

--- sd300w22sdlabs/Program.cs
+++ sd300w22sdlabs/Program.cs
@@ -5,43 +5,63 @@
 
 Dictionary<Product, int> inventory = new Dictionary<Product, int>
 {
-    {new Product("Chocolate-covered Beans", 2, "A12", "BARCODE1"),  3}
+    {new Product("Chocolate-covered Beans", 2, "A12"),  3}
 };
 
-VendingMachine vendingMachine = new VendingMachine(moneyFloat, inventory);
+VendingMachine vendingMachine = new VendingMachine(moneyFloat, inventory, "BARCODE1");
 
-vendingMachine.StockItem(new Product("Fried Spider", 4, "A13", "BARCODE2"), 4);
-foreach (KeyValuePair<Product, int> entity in inventory)
+try
 {
-    Console.WriteLine($"{entity.Key.Name}: {entity.Value}");
+    vendingMachine.StockItem(new Product("Fried Spider", 4, "A13"), 4);
+    foreach (KeyValuePair<Product, int> entity in inventory)
+    {
+        Console.WriteLine($"{entity.Key.Name}: {entity.Value}");
+    }
+
+    vendingMachine.StockFloat(5, 2);
+    foreach (KeyValuePair<int, int> entity in moneyFloat)
+    {
+        Console.WriteLine($"${entity.Key}: {entity.Value}");
+    }
+
+    Console.WriteLine(vendingMachine.VendItem("A12", new List<int> { 1 }));
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
 }
 
-vendingMachine.StockFloat(5, 2);
-foreach (KeyValuePair<int, int> entity in moneyFloat)
-{
-    Console.WriteLine($"${entity.Key}: {entity.Value}");
-}
-
-Console.WriteLine(vendingMachine.VendItem("A12", new List<int> { 5 }));
 
 class VendingMachine
 {
     public int SerialNumber { get; set; }
     private Dictionary<int, int> MoneyFloat { get; set; }
     private Dictionary<Product, int> Inventory { get; set; }
+    public string Barcode { get; }
 
     private static int SerialNumberCounter = 1;
 
-    public VendingMachine(Dictionary<int, int> moneyFloat, Dictionary<Product, int> inventory)
+    public VendingMachine(Dictionary<int, int> moneyFloat, Dictionary<Product, int> inventory, string barcode)
     {
+        if (barcode.Length == 0)
+        {
+            throw new Exception("VendingMachine barcode cannot be empty");
+        }
+
         SerialNumber = SerialNumberCounter;
         SerialNumberCounter++;
         MoneyFloat = moneyFloat;
         Inventory = inventory;
+        Barcode = barcode;
     }
 
     public string StockItem(Product product, int quantity)
     {
+        if (quantity < 0)
+        {
+            throw new Exception("Quantity cannot be negative");
+        }
+
         Inventory.Add(product, quantity);
 
         return string.Join(", ", Inventory.Select(e => $"{e.Key}: {e.Value}"));
@@ -60,26 +80,26 @@ class VendingMachine
 
         if (product == null)
         {
-            return "Error: Does not exist";
+            throw new Exception($"Product with the code {code} does not exist");
         }
 
         int quantity = Inventory[product];
 
         if (quantity == 0)
         {
-            return "Error: Out of stock";
+            throw new Exception($"Product with the code {code} is out of stock");
         }
 
         if (product.Price > money.Sum())
         {
-            return "Error: Insufficient money provided";
+            throw new Exception("Insufficient money provided");
         }
 
         int change = money.Sum() - product.Price;
 
         if (MoneyFloat.Sum(e => e.Key * e.Value) < change)
         {
-            return "Error: Insufficient change";
+            throw new Exception("The machine does not have enough money to return the change");
         }
 
         int temp = change;
@@ -114,13 +134,24 @@ class Product
     public string Name { get; set; }
     public int Price { get; set; }
     public string Code { get; set; }
-    public string Barcode { get; }
 
-    public Product(string name, int price, string code, string barcode)
+    public Product(string name, int price, string code)
     {
+        if (name == null || name.Length == 0)
+        {
+            throw new Exception("Product name cannot be null or empty");
+        }
+        if (code == null || code.Length == 0)
+        {
+            throw new Exception("Product code cannot be null or empty");
+        }
+        if (price <= 0)
+        {
+            throw new Exception("Product price cannot be negative or zero");
+        }
+
         Name = name;
         Price = price;
         Code = code;
-        Barcode = barcode;
     }
 }
